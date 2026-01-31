1a7f9c3350ba634e793c42b31c7f73f2fd71c262
Jim Schubert <james.schubert@gmail.com>
2026-01-31 10:47:24 -0500

    fix: Kong version templating

--- cmd/main.go
+++ cmd/main.go
@@ -10,25 +10,35 @@ import (
 	log "github.com/sirupsen/logrus"
 )
 
-var version = ""
+var version = "dev"
 
 // noinspection GoUnusedGlobalVariable
-var date = ""
-var commit = ""
-var projectName = ""
+var date = "1970-01-01T00:00:00Z"
+var commit = "unknown"
+var projectName = "delete-artifacts"
 
 var opts struct {
-	Owner          *string `short:"o" help:"GitHub Owner/Org name" env:"GITHUB_ACTOR"`
-	Repo           *string `short:"r" help:"GitHub Repo name" env:"GITHUB_REPO"`
-	RunId          *int64  `short:"i" name:"run-id" help:"The workflow run id from which to delete artifacts" optional:""`
-	MinBytes       int64   `name:"min" help:"Minimum size in bytes. Artifacts greater than this size will be deleted." default:"50000000"`
-	MaxBytes       *int64  `name:"max" help:"Maximum size in bytes. Artifacts less than this size will be deleted" optional:""`
-	Name           string  `short:"n" help:"Artifact name to be deleted" default:""`
-	Pattern        string  `short:"p" help:"Regex pattern (POSIX) for matching artifact name to be deleted" default:""`
-	ActiveDuration string  `short:"a" name:"active" help:"Consider artifacts as 'active' within this time frame, and avoid deletion. Duration formatted such as 23h59m." default:""`
-	LogLevel       string  `short:"l" name:"log-level" help:"Log level (trace, debug, info, warn, error, fatal, panic)" env:"LOG_LEVEL" default:"info"`
-	DryRun         bool    `name:"dry-run" help:"Dry-run that does not perform deletions"`
-	Version        bool    `short:"v" help:"Display version information"`
+	Owner          *string     `short:"o" help:"GitHub Owner/Org name" env:"GITHUB_ACTOR"`
+	Repo           *string     `short:"r" help:"GitHub Repo name" env:"GITHUB_REPO"`
+	RunId          *int64      `short:"i" name:"run-id" help:"The workflow run id from which to delete artifacts" optional:""`
+	MinBytes       int64       `name:"min" help:"Minimum size in bytes. Artifacts greater than this size will be deleted." default:"50000000"`
+	MaxBytes       *int64      `name:"max" help:"Maximum size in bytes. Artifacts less than this size will be deleted" optional:""`
+	Name           string      `short:"n" help:"Artifact name to be deleted" default:""`
+	Pattern        string      `short:"p" help:"Regex pattern (POSIX) for matching artifact name to be deleted" default:""`
+	ActiveDuration string      `short:"a" name:"active" help:"Consider artifacts as 'active' within this time frame, and avoid deletion. Duration formatted such as 23h59m." default:""`
+	LogLevel       string      `short:"l" name:"log-level" help:"Log level (trace, debug, info, warn, error, fatal, panic)" env:"LOG_LEVEL" default:"info"`
+	DryRun         bool        `name:"dry-run" help:"Dry-run that does not perform deletions"`
+	Version        VersionFlag `short:"v" help:"Display version information"`
+}
+
+type VersionFlag string
+
+func (v VersionFlag) Decode(ctx *kong.DecodeContext) error { return nil }
+func (v VersionFlag) IsBool() bool                         { return true }
+func (v VersionFlag) BeforeApply(app *kong.Kong, vars kong.Vars) error {
+	fmt.Println(vars["version"])
+	app.Exit(0)
+	return nil
 }
 
 func main() {
@@ -36,12 +46,10 @@ func main() {
 		kong.Name(projectName),
 		kong.Description("Delete GitHub Actions artifacts"),
 		kong.UsageOnError(),
+		kong.Vars{
+			"version": fmt.Sprintf("%s (%s)[%s]", version, commit, date),
+		},
 	)
-
-	if opts.Version {
-		fmt.Printf("%s %s (%s)\n", projectName, version, commit)
-		ctx.Exit(0)
-	}
 
 	initLogging(opts.LogLevel)
 
@@ -55,15 +63,9 @@ func main() {
 		opts.Pattern,
 		opts.ActiveDuration,
 		opts.DryRun)
-	if err != nil {
-		log.WithError(err).Errorf("unable to construct application with specific parameters.")
-		return
-	}
+	ctx.FatalIfErrorf(err, "unable to construct application with specific parameters.")
 	err = application.Run()
-	if err != nil {
-		log.WithError(err).Errorf("execution failed.")
-		return
-	}
+	ctx.FatalIfErrorf(err, "execution failed.")
 
 	log.Info("Run complete.")
 }
